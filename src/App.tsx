48b40832a2b31182188fbea2781306b285c534e4
47JMC <mjabir.5b@gmail.com>
2026-04-03 14:36:42 +0530

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl flex justify-center items-center font-bold">
-        Yo
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" />
+    </Routes>
   );
 }
 
