17b9b74911c30695dea233c4c2bcea5da8ffae7e
47JMC <mjabir.5b@gmail.com>
2026-04-03 11:39:48 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl flex justify-center items-center font-bold">
+        Yo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
