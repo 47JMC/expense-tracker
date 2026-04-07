5dc2682d90a7b7f2abd03f646339ae4eb2d900ef
47JMC <mjabir.5b@gmail.com>
2026-04-07 08:17:31 +0530

    adjust styles

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -11,8 +11,8 @@ function NavbarLink({ path, content }: Props) {
       to={path}
       className={({ isActive }) =>
         isActive
-          ? "transition-all font-['Outfit'] text-green-500"
-          : "transition-all font-['Outfit'] relative text-sky-4000 hover:text-sky-600 after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-indigo-600 hover:after:w-full after:transition-all after:duration-300"
+          ? "transition-all font-['Outfit'] text-green-400"
+          : "transition-all font-['Outfit'] relative text-sky-400 hover:text-sky-600 after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-indigo-600 hover:after:w-full after:transition-all after:duration-300"
       }
     >
       {content}
